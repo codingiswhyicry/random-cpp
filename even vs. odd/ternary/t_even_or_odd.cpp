2c1886a0efeae02312bfb884d621a62a2f348e1f
thecodingone <amanda.southworth@myds.us>
2017-02-08 13:42:15 -0800

    added ternary option and even or odd folder

---
+++ even vs. odd/ternary/t_even_or_odd.cpp
@@ -0,0 +1,9 @@
+
+string even_odd(int num) {
+        
+        string answer; 
+        
+        (num % 2 == 0) ? answer = "even" : answer = "odd"; 
+        
+        return answer; 
+    }
---
+++ even vs. odd/ternary/t_even_or_odd.h
@@ -0,0 +1,6 @@
+#ifndef EVEN_VS_ODD
+#def EVEN_VS_ODD
+
+    string even_odd(int num);
+
+#endif
